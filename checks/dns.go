710cb7d517ee3747d3bc4a2f74b027a3b959b716
Binaryfigments <s.broekhoven@gmail.com>
2019-10-19 15:18:40 +0200

    removed extra numbers.

--- checks/dns.go
+++ checks/dns.go
@@ -15,11 +15,11 @@ func checkDomainState(domain string, nameserver string) string {
 	if err == nil {
 		switch in.MsgHdr.Rcode {
 		case dns.RcodeServerFailure:
-			return "500, 502, The name server encountered an internal failure while processing this request (SERVFAIL)"
+			return "502, The name server encountered an internal failure while processing this request (SERVFAIL)"
 		case dns.RcodeNameError:
-			return "500, 503, Some name that ought to exist, does not exist (NXDOMAIN)"
+			return "503, Some name that ought to exist, does not exist (NXDOMAIN)"
 		case dns.RcodeRefused:
-			return "500, 505, The name server refuses to perform the specified operation for policy or security reasons (REFUSED)"
+			return "505, The name server refuses to perform the specified operation for policy or security reasons (REFUSED)"
 		default:
 			return "OK"
 		}
